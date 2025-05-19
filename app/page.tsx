0a831e62766d648b75240d7458e8dccd54f76758
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-19 12:00:40 +0400

    set up clerk and basic pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Camera } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button>default button</Button>
-      <Button variant="outline" size="icon">
-        <Camera />
-      </Button>
-    </div>
+    <main>
+      <header className="max-w-6xl mx-auto px-4 sm:px-8 py-6 ">
+        <Image src={"/logo.svg"} alt="logo" width={120} height={40} />
+      </header>
+      <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-22 grid lg:grid-cols-[1fr_400px] items-center">
+        <div>
+          <h1 className="capitalize text-4xl md:text-7xl font-bold">
+            job <span className="text-primary">tracking</span> app
+          </h1>
+          <p className="leading-loose max-w-md mt-4 ">
+            I am baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          <Button asChild className="mt-4">
+            <Link href="/add-job">Get Started</Link>
+          </Button>
+        </div>
+        <Image
+          src={"/main.svg"}
+          alt="landing"
+          width={400}
+          height={355}
+          className="hidden lg:block "
+        />
+      </section>
+    </main>
   );
 }
