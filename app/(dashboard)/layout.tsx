e6b19ea6d44985292be706f8a0c151b6213ddbd9
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-19 12:28:52 +0400

    added layout styles for dashboard pages and added shared layout components

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,16 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main>{children}</main>;
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
+export default function Layout({ children }: React.PropsWithChildren) {
+  return (
+    <main className="grid lg:grid-cols-5">
+      <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
+        <Sidebar />
+      </div>
+      <div className="lg:col-span-4">
+        <Navbar />
+        <div className="py-15 px-4 sm:px-8 lg:px-16">{children}</div>
+      </div>
+    </main>
+  );
 }
