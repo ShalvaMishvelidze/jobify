9fee0b5fcc027635112098397a092150b210bb39
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-20 15:36:52 +0400

    added complex pagination to the app

--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -4,6 +4,7 @@ import { getAllJobsAction } from "@/utils/actions";
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
 import JobCard from "./JobCard";
+import ButtonContainer from "./ButtonContainer";
 
 const JobsList = () => {
   const searchParams = useSearchParams();
@@ -18,17 +19,24 @@ const JobsList = () => {
   });
 
   const jobs = data?.jobs || [];
+  // add
+  const count = data?.count || 0;
+  const page = data?.page || 0;
+  const totalPages = data?.totalPages || 0;
 
-  if (isPending) {
-    return <h2 className="text-xl">Please wait...</h2>;
-  }
+  if (isPending) return <h2 className="text-xl">Please Wait...</h2>;
 
-  if (jobs.length < 1) {
-    return <h2 className="text-xl">No jobs found</h2>;
-  }
-
+  if (jobs.length < 1) return <h2 className="text-xl">No Jobs Found...</h2>;
   return (
     <>
+      <div className="flex items-center justify-between mb-8">
+        <h2 className="text-xl font-semibold capitalize ">
+          {count} jobs found
+        </h2>
+        {totalPages < 2 ? null : (
+          <ButtonContainer currentPage={page} totalPages={totalPages} />
+        )}
+      </div>
       <div className="grid md:grid-cols-2 gap-8">
         {jobs.map((job) => {
           return <JobCard key={job.id} job={job} />;
